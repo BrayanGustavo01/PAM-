06ba455f6fa3a66c4df635ac72f03a9e7def7683
Gustavo <143816558+BrayanGustavo01@users.noreply.github.com>
2024-09-27 07:47:51 -0300

    Add files via upload

---
+++ Calcular.js
@@ -0,0 +1,24 @@
+
+var v1 = 'Valor 1';
+var v2 = 'Valor 2';
+
+ function resultadoSoma(){
+        return v1+v2;
+        console.log();
+ }
+
+ function resultadoSubtração(){
+        return v1+v2;
+        console.log();
+}
+
+ function resultadoDivisão(){
+        return v1+v2;
+        console.log();
+ }
+
+ function resultadoMultiplicação(){
+        return v1+v2;
+        console.log();
+}
+
---
+++ Script2.js
@@ -0,0 +1,8 @@
+
+function imprimeOla(){
+    var nome = 'Danilo';
+    console.log(m);
+    console.log(nome);
+}
+
+imprimeOla();
