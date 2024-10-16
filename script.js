a741aa2f44a9088b6971713a98846eb3e52469ea
Gustavo <143816558+BrayanGustavo01@users.noreply.github.com>
2024-10-16 08:40:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function iniciar() {
-    alert('Abra o console do navegador (F12 ou Ctrl + Shift + I) e digite um número de 3 a 10.');
     mostrarPrompt();
 }
 
 function mostrarPrompt() {
-    let num = prompt('Digite um número de 3 a 10 para selecionar uma forma geométrica:');
+    let num = prompt('Digite o número de lados que a sua forma tem: ');
     const formaS = parseInt(num);
     
     formasGPagina(formaS); 
@@ -15,53 +14,57 @@ function formasGPagina(a) {
 
     
     switch (a) {
+        case 1:
+        case 2:
+            alert("Esta forma não existe.")
+            break;
         case 3:
             forma = 'Triângulo';
             area = 'A = b * h / 2';
             perimetro = 'P = a + b + c';
-            imagemUrl = 'img/img-3.png';
+            imagemUrl = 'imagens/triangulo.png';
             break;
         case 4:
             forma = 'Quadrado';
             area = 'A = L²';
             perimetro = 'P = L * 4';
-            imagemUrl = 'img/img-4.png';
+            imagemUrl = 'imagens/quadrado.png';
             break;
         case 5:
             forma = 'Pentágono';
             area = 'A = 3L² √3 / 2';
             perimetro = 'P = 5 * L';
-            imagemUrl = 'img/img-5.png';
+            imagemUrl = 'imagens/pentagono.png';
             break;
         case 6:
             forma = 'Hexágono';
             area = 'A = (3√3 / 2) * L²';
             perimetro = 'P = 6 * L';
-            imagemUrl = 'img/img-6.png';
+            imagemUrl = 'imagens/hexagono.png';
             break;
         case 7:
             forma = 'Heptágono';
             area = 'A = (7 / 4) * L² * cot(π / 7)';
             perimetro = 'P = 7 * L';
-            imagemUrl = 'img/img-7.png'; 
+            imagemUrl = 'imagens/heptagono.png'; 
             break;
         case 8:
             forma = 'Octágono';
             area = 'A = 2 * (1 + √2) * L²';
             perimetro = 'P = 8 * L';
-            imagemUrl = 'img/img-8.png'; 
+            imagemUrl = 'imagens/octagono.png'; 
             break;
         case 9:
             forma = 'Eneágono';
             area = 'A = (9 / 4) * L² * cot(π / 9)';
             perimetro = 'P = 9 * L';
-            imagemUrl = 'img/img-9.png'; 
+            imagemUrl = 'imagens/eneagono.png'; 
             break;
         case 10:
             forma = 'Decágono';
             area = 'A = (5 / 2) * L² * √(5 + 2√5)';
             perimetro = 'P = 10 * L';
-            imagemUrl = 'img/img-10.png';
+            imagemUrl = 'imagens/decagono.png';
             break;
         default:
             console.log('Forma Geométrica não implementada');
@@ -69,5 +72,13 @@ function formasGPagina(a) {
     }
  
     mostrarForma(forma, area, perimetro, imagemUrl);
+
+}
+
+function mostrarForma(forma, area, perimetro, imagemUrl) {
+    document.getElementById('formaNome').innerText = forma;
+    document.getElementById('areaCalculo').innerText = `Como calcular a Área: ${area}`;
+    document.getElementById('perimetroCalculo').innerText = `Como calcular o Perímetro: ${perimetro}`;
+    document.getElementById('lord').src = imagemUrl;
 }
 
